26697c7ce7f351e8961ce6801f2ef637d817ed33
szymonkowalskiPL <74612883+szymonkowalskiPL@users.noreply.github.com>
2021-03-22 18:55:52 +0100

    test na nierowne sekwencje

--- src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -28,5 +28,12 @@ class SimilarityFinderTest {
         int [] seq2= {0,1,2,3};
         assertEquals(1, finder.calculateJackardSimilarity(seq1, seq2));
     }
+    @Test
+    public void notEqualSequencesTest()
+    {
+        int [] seq1= {1,2,3,4};
+        int [] seq2= {5,6,7,8};
+        assertEquals(0.0, finder.calculateJackardSimilarity(seq1, seq2));
+    }
 
 }
