fe07c2721f2c17f4c2eae0b999ca8b91a123e5fd
adamus.r <adamus.r@gmail.com>
2021-03-22 06:44:24 +0100

    test class added

---
+++ src/test/java/edu/iis/mto/similarity/SimilarityFinderTest.java
@@ -0,0 +1,14 @@
+package edu.iis.mto.similarity;
+
+import static org.junit.Assert.fail;
+
+import org.junit.Test;
+
+public class SimilarityFinderTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
